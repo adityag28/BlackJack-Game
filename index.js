1501fb12d6a93c7cec22ec552c6cc234877f336d
adityag28 <adityag0028@gmail.com>
2024-08-02 10:04:47 +0530

    Rename & added new  function name.
    I have rename the cardStart() function to renderStart() function. I have created new function as gameStart() and inside it i have called renderStart() function.

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ let cardOne = 10;
 let cardTwo = 1;
 sum = cardOne + cardTwo;
 let message = ""; //created message with empty string
-let cardBt = document.querySelector("#card-bt"); //Asked for a html element by its id.
-let cardMsg = document.querySelector("#msg-cn"); //Asked for a html element by its id.
-let cardSum = document.querySelector("#sum-cn"); //Asked for a html element by its id.
-function cardStart() {
-  cardBt.textContent += cardOne + " " + cardTwo; //added cardOne & cardTwo values infront of the  card.
-  cardSum.textContent += sum; //added sum value infront of  the  sum.
+let cardBt = document.querySelector("#card-bt"); //Asked for a html element by its id selector.
+let cardMsg = document.querySelector("#msg-cn"); //Asked for a html element by its id selector.
+let cardSum = document.querySelector("#sum-cn"); //Asked for a html element by its id selector.
+
+function gameStart() {
+  //added this function for calling renderStart() function
+  renderStart();
+}
+function renderStart() {
+  //changed name from cardStart to renderStart because it render the conditon inside the function.
+  cardBt.textContent = "Cards: " + cardOne + " " + cardTwo; //added cardOne & cardTwo values infront of the  card.
+  cardSum.textContent = "Sum: " + sum; //added sum value infront of  the  sum.
   if (sum < 20) {
     //it will check if the sum is less than 21 if  it is true, then it will give the message as output.
     message = "Do you want to draw a new card ?";
@@ -19,4 +25,11 @@ function cardStart() {
     message = "You're out of the game!";
   }
   cardMsg.textContent = message; //by using this we are displaying the message of the true condition.
+}
+
+function newCard() {
+  //  added this function to add new card value inside the sum variable onclick of new card
+  let cardThree = 5;
+  sum += cardThree;
+  renderStart(); // call this card to re-run the code of this function
 }
